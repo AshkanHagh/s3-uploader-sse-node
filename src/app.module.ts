2fbf72037a4f43668fa87c47663aa3b1edc24758
Ashkan <AshkanHagh01@gmail.com>
2026-02-27 05:45:26 +0330

    feat(app): switch NestJS adapter from Express to Fastify and configure modules
    - Install required packages
    - Configure modules for Fastify
    - Migrate NestJS from Express to Fastify adapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,35 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { ContextLoggerModule } from "nestjs-context-logger";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ContextLoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.LOG_LEVEL || "info",
+        autoLogging: false,
+        quietReqLogger: true,
+        transport:
+          process.env.NODE_ENV !== "production"
+            ? {
+                target: "pino-pretty",
+                options: {
+                  colorize: false,
+                  ignore: "pid,hostname,context,component",
+                },
+              }
+            : undefined,
+      },
+      forRoutes: ["*path"],
+    }),
+    EventEmitterModule.forRoot({
+      global: true,
+      wildcard: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.enableCors({ origin: process.env.CORS_ORIGIN });
+  app.setGlobalPrefix("api");
+  app.enableShutdownHooks();
+
+  await app.register(import("@fastify/multipart"));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
